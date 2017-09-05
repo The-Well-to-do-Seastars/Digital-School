5798e569162a74916aae5e976efc81182d5678b2
Dankatta <dani.dvh@gmail.com>
2017-09-05 15:22:02 +0300

    basic news feed viewer

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'dschool-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent {
+   news: FirebaseListObservable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    this.news = db.list('/news');
   }
-
 }
