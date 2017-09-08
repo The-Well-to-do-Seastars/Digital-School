b19fa4e0455e4a55a35da9f0c8769d9578ca4fd4
puhabg <puhabg@live.com>
2017-09-08 22:45:14 +0300

    Adds toastr notification

--- src/app/home/add-news/add-news.component.ts
+++ src/app/home/add-news/add-news.component.ts
@@ -1,8 +1,9 @@
+import { ToasterService } from 'angular2-toaster';
 import { NewsArticle } from './../../shared/models/news-article';
 import { NewsService } from './../../core/news.service';
 import { Component, OnInit, SkipSelf } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dschool-add-news',
@@ -12,21 +13,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AddNewsComponent implements OnInit {
   public addNewsForm: FormGroup;
 
-  constructor(private newsService: NewsService) { }
+  constructor(
+    private newsService: NewsService,
+    private toasterService: ToasterService,
+    private router: Router
+  ) { }
 
   onSubmit() {
     const newsArticle: NewsArticle = new NewsArticle();
     newsArticle.content = this.addNewsForm.controls['content'].value;
     newsArticle.title = this.addNewsForm.controls['title'].value;
     this.newsService.createNews(newsArticle);
+    this.toasterService.pop('success', 'The Article is posted');
+    this.router.navigate(['']);
   }
 
   showValidationMessades() {
-    for (var key in this.addNewsForm.controls) {
-      this.addNewsForm.controls[key].markAsTouched();
+    for (const key in this.addNewsForm.controls) {
+      if (this.addNewsForm.controls.hasOwnProperty(key)) {
+        this.addNewsForm.controls[key].markAsTouched();
+      }
     }
   }
-  
   ngOnInit() {
     const title = new FormControl(null, Validators.required);
     const content = new FormControl(null, Validators.required);
