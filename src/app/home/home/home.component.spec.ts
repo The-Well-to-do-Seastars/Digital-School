0ad7a7ac4343f86a4ad32449c875ea587e424f7e
Asparuh <puhabg@live.com>
2017-09-10 19:56:17 +0300

    Setups tests

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -1,25 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { EditNewsComponent } from './../edit-news/edit-news.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HomeModule } from './../home.module';
+import { HomeRoutingModule } from './../home-routing.module';
+import { Router, RouterModule } from '@angular/router';
+import { NewsService } from './../../core/news.service';
+import { UserService } from './../../core/user.service';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { RouterLinkStubDirective, RouterOutletStubComponent } from '../../../test/router-stubs';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../../../environments/environment';
+
 
 import { HomeComponent } from './home.component';
+import { TimeAgoPipe } from 'time-ago-pipe/time-ago-pipe';
 
-describe('HomeComponent', () => {
+
+class RouterStub {
+  navigateByUrl(url: string) { return url; }
+}
+
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [ AngularFireModule.initializeApp(environment.firebase), RouterTestingModule ],
+      declarations: [ HomeComponent, TimeAgoPipe ],
+      providers: [UserService, NewsService, AngularFireDatabase, AngularFireAuth ]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should be created', inject([Router], (router: Router)  => {
     expect(component).toBeTruthy();
-  });
+  }));
 });
